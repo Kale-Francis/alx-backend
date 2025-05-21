dddc5aaf23f46234f695456097a7ac182d0574c7
Kale-Francis <kalefrancis989@gmail.com>
2025-05-21 00:57:05 +0000

    Implimented a mock for this task

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Flask app with mock user login"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@babel.localeselector
+def get_locale():
+    """Select locale from URL parameter or request headers"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+def get_user():
+    """Return user dictionary or None"""
+    user_id = request.args.get('login_as')
+    if user_id and user_id.isdigit():
+        return users.get(int(user_id))
+    return None
+
+@app.before_request
+def before_request():
+    """Set user as global before each request"""
+    g.user = get_user()
+
+@app.route('/')
+def index():
+    """Render the index page with translations and user info"""
+    return render_template('5-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
